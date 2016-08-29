3da6db23d54500b3a1bd1603ae5d6df15abeafca
benjaminsw <benjaminsw@live.com>
2016-08-29 16:12:11 +0100

    successfully read input

--- server.R
+++ server.R
@@ -1,17 +1,19 @@
 library(shiny)
 library("xlsx")
 df <- read.xlsx("Helpline.xlsx", 2, header = TRUE)
+df$NA. <- as.character(df$NA.)
+df[is.na(df)] <- 0
 # Define server logic for slider examples
 shinyServer(function(input, output) {
   
-  # Reactive expression to compose a data frame containing all of the values
-  sliderValues <- reactive({
-    
-    
-  }) 
-  
-  # Show the values using an HTML table
-  output$values <- renderTable({
-    sliderValues()
+  sliderMonth <- reactiveValues()
+  observe({
+    full.date <- as.POSIXct(input$slider, tz="GMT")
+    sliderMonth$Month <- as.character(monthStart(full.date))
+    #query <- as.character(format(full.date, "%Y-%m"))
+    result <- subset(df, df$NA.==sliderMonth$Month,)
+    print(result)
   })
+  
+  output$SliderText <- renderText({sliderMonth$Month})
 })
--- ui.R
+++ ui.R
@@ -2,13 +2,13 @@
 # Define UI for slider demo application
 shinyUI(
   pageWithSidebar(
-  headerPanel("Shelter Data"),
-  sidebarPanel(
-    sliderInput("slider", "Time", min = as.Date("2015-07-01"),max =as.Date("2016-06-01"),value=as.Date("2015-07-01"),timeFormat="%b %Y", animate=TRUE)
-  ),
-  mainPanel(
-    tableOutput("values")
-  )
+    headerPanel("Shelter Scotland Data"),
+    sidebarPanel(
+      sliderInput("slider", "Time", min = as.Date("2015-07-01"),max =as.Date("2016-06-01"),value=as.Date("2015-07-01"),timeFormat="%b %Y", animate=TRUE)
+    ),
+    mainPanel(
+      textOutput("SliderText")
+    )
   )
   
 )
