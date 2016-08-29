43ba195c0c8c1e817f3d113a889fd96f90e1a245
benjaminsw <benjaminsw@live.com>
2016-08-29 18:02:26 +0100

    done query data

--- server.R
+++ server.R
@@ -1,7 +1,8 @@
 library(shiny)
 library("xlsx")
+library(reshape2)
 df <- read.xlsx("Helpline.xlsx", 2, header = TRUE)
-df$NA. <- as.character(df$NA.)
+#df$NA. <- as.character(df$NA.)
 df[is.na(df)] <- 0
 # Define server logic for slider examples
 shinyServer(function(input, output) {
@@ -9,11 +10,18 @@ shinyServer(function(input, output) {
   sliderMonth <- reactiveValues()
   observe({
     full.date <- as.POSIXct(input$slider, tz="GMT")
-    sliderMonth$Month <- as.character(monthStart(full.date))
-    #query <- as.character(format(full.date, "%Y-%m"))
-    result <- subset(df, df$NA.==sliderMonth$Month,)
+    query <- as.character(format(full.date, "%Y-%m-%d"))
+    #print(query)
+    result <- subset(df, df$NA.==query,)
     print(result)
+    #print(subset(df, df$NA.=="2015-07-01",))
+    #resultLong <- melt(result, id.vars = "NA.",value.name = "value")
+    #print(sliderMonth$Month)
   })
   
   output$SliderText <- renderText({sliderMonth$Month})
+  output$BBplot <- renderPlot({
+    d <- data()
+    plot(d$speed, d$dist)
+  })
 })
