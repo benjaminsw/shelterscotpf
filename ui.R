9c74af9e593072f9d9046fe2e701efb5c5d078ae
benjaminsw <benjaminsw@live.com>
2016-08-29 18:35:29 +0100

    added width in ui to 200%

--- ui.R
+++ ui.R
@@ -7,7 +7,7 @@ shinyUI(
       sliderInput("slider", "Time", min = as.Date("2015-07-01"),max =as.Date("2016-06-01"),value=as.Date("2015-07-01"),timeFormat="%b %Y", animate=TRUE)
     ),
     mainPanel(
-      plotOutput("BBplot")
+      plotOutput("BBplot",  width = "200%")
     )
   )
   
