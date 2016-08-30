160057c0fbab45be8a00546cc52a4ae9c4b76044
benjaminsw <benjaminsw@live.com>
2016-08-30 11:33:13 +0100

    tidied up server

--- server.R
+++ server.R
@@ -1,20 +1,16 @@
 library(shiny)
+library(ggplot2)
 library("xlsx")
 library(reshape2)
 df <- read.xlsx("Helpline.xlsx", 2, header = TRUE)
-#df$NA. <- as.character(df$NA.)
 df[is.na(df)] <- 0
 # Define server logic for slider examples
 shinyServer(function(input, output) {
   output$barPlot <- renderPlot({
       full.date <- as.POSIXct(input$slider, tz="GMT")
       query <- as.character(format(full.date, "%Y-%m-01"))
-      #print(query)
       result <- subset(df, df$NA.==query,)
-      #result <- subset(df, df$NA.=="2015-07-01",)
-      #print(result)
       resultLong <- melt(result, id.vars = "NA.",value.name = "value")
-      print(resultLong)
     ggplot(resultLong, aes(x=variable, y=value)) + 
       geom_bar(stat="identity", fill="steelblue") + ylab("Value")+xlab("Matrices") +
       geom_text(aes(label=value),angle = 90, hjust = 2, color="white", size=12) +
