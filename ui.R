73ddea0319f9dabcd32cfba4267e17678c57d17d
benjaminsw <benjaminsw@live.com>
2016-08-30 10:41:43 +0100

    added tabs

--- ui.R
+++ ui.R
@@ -7,7 +7,12 @@ shinyUI(
       sliderInput("slider", "Time", min = as.Date("2015-07-01"),max =as.Date("2016-06-01"),value=as.Date("2015-07-01"),timeFormat="%b %Y", animate=TRUE)
     ),
     mainPanel(
-      plotOutput("BBplot",  width = "200%")
+      tabsetPanel(
+        tabPanel("Plot", plotOutput("barPlot",  width = "200%")),
+        tabPanel("Table", tableOutput("table")),
+        tabPanel("Summary", verbatimTextOutput("summary"))
+      )
+      
     )
   )
   
